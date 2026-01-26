8253e1d67c490e3dbf788954b168adab8d6e8c1e
Aurele Gnanha <136912485+Tohokantche@users.noreply.github.com>
2026-01-26 18:39:07 +0800

    Update agents.py

--- agents.py
+++ agents.py
@@ -2,6 +2,7 @@ from crewai import Agent
 #from langchain_openai import ChatOpenAI
 #from langchain_groq import ChatGroq  # Import Groq client
 from langchain_openai import ChatOpenAI
+from openai import OpenAI
 import os
 from crewai_tools import SerperDevTool,WebsiteSearchTool, ScrapeWebsiteTool 
 
@@ -22,7 +23,7 @@ class ResearchCrewAgents:
        #  self.gpt3_5_turbo_0125 = ChatOpenAI(model_name="gpt-3.5-turbo-0125", temperature=0.7)
        #  self.gpt3_5_turbo_1106 = ChatOpenAI(model_name="gpt-3.5-turbo-1106", temperature=0.7)
        #  self.gpt3_5_turbo_instruct = ChatOpenAI(model_name="gpt-3.5-turbo-instruct", temperature=0.7)
-        self.deepseek = ChatOpenAI(model_name="deepseek/deepseek-r1-0528:free", 
+        self.deepseek = OpenAI(model_name="deepseek/deepseek-r1-0528:free", 
                                    api_key=os.environ.get("OPENAI_API_KEY"),
                                    base_url="https://openrouter.ai/api/v1", 
                                    temperature=0.7)
